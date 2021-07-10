d026ed6f19d6090bc5308f8400c92929d4081a00
Nick Billhardt <nickbillhardt@ymail.com>
2021-07-10 15:56:16 -0400

    people component completed

---
+++ js/components/ContactForm.js
@@ -0,0 +1,9 @@
+export default function ContactForm(){
+    return `
+    <form>
+    <h4>Contact</h4>
+    <input type="text" placeholder="name" />
+    <input type="text" placeholder="email" />
+    </form>
+    `
+}
--- js/index.js
+++ js/index.js
@@ -1,12 +1,43 @@
-import Header from './components/Header.js'
+import Footer from "./components/Footer.js";
+import Header from "./components/Header.js";
+import HomePage from "./pages/HomePage.js";
+import apiActions from "./api-actions/api-actions.js";
+import PeoplePage from "./pages/PeoplePage.js";
 
 buildPage();
 
 function buildPage() {
-    header();
+  header();
+  footer();
+  navigateToHomePage();
+  renderPeopleInfoList();
 }
 
 function header() {
-    const headerElement = document.querySelector('.header');
-    headerElement.innerHTML = Header();
+  const headerElement = document.querySelector(".header");
+  headerElement.innerHTML = Header();
+}
+
+function footer() {
+  const footerElement = document.querySelector(".footer");
+  footerElement.innerHTML = Footer();
+}
+
+function navigateToHomePage() {
+  const homeButton = document.querySelector(".nav__list_home");
+  homeButton.addEventListener("click", () => {
+    const app = document.querySelector("#app");
+    app.innerHTML = HomePage();
+  });
+}
+
+function renderPeopleInfoList() {
+  console.log("render people function working");
+  const peopleButton = document.querySelector(".nav__list_people");
+  peopleButton.addEventListener("click", () => {
+    const app = document.querySelector("#app");
+    apiActions.getRequest("https://swapi.dev/api/people/", (people) => {
+     app.innerHTML = PeoplePage(people);
+    });
+  });
 }
